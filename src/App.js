9e16dcc61364a285e337775cdfb590a51885bc96
Radicals <cclose27@gmail.com>
2020-07-14 09:52:02 +1000

    Set up MongoDB connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+const MongoClient = require('mongodb').MongoClient
+const url = process.env.MONGODB_URI || "mongodb://localhost:3000"
 
 function App() {
   return (
     <div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
